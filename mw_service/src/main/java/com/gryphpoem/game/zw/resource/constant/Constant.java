65ff93f05f0350abb516a540aeebc4faba046ce6
zhangpeng <0807zp123>
2022-08-16 18:17:29 +0800

    武将列传 - 新增战力对比类型

--- mw_service/src/main/java/com/gryphpoem/game/zw/resource/constant/Constant.java
+++ mw_service/src/main/java/com/gryphpoem/game/zw/resource/constant/Constant.java
@@ -1405,9 +1405,9 @@ public final class Constant {
         int OTHER = 9;//科技、其它
         int SEASON = 10;//宝具
         int CASTLE_SKIN = 11;// 城堡皮肤
-        int TREASURE_WARE = 13;//宝具
         int TOTEM = 12;//阵法图腾
-        int HERO_BIOGRAPHY = 13;    // 武将列传
+        int TREASURE_WARE = 13;//宝具
+        int HERO_BIOGRAPHY = 14;    // 武将列传
     }
 
     /**
