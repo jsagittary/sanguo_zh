4e8c9dcd7140cfaeb0ea15dba9ec57547e366709
zhangpeng <0807zp123>
2022-09-13 21:44:38 +0800

    战报 - 获取战报handler

--- mw_game/src/main/java/com/gryphpoem/game/zw/handler/client/mail/GetMailReportHandler.java
+++ mw_game/src/main/java/com/gryphpoem/game/zw/handler/client/mail/GetMailReportHandler.java
@@ -1,10 +1,14 @@
 package com.gryphpoem.game.zw.handler.client.mail;
 
 import com.gryphpoem.game.zw.core.common.DataResource;
+import com.gryphpoem.game.zw.core.exception.MwException;
 import com.gryphpoem.game.zw.core.handler.AsyncGameHandler;
 import com.gryphpoem.game.zw.manager.PlayerDataManager;
 import com.gryphpoem.game.zw.pb.GamePb5;
+import com.gryphpoem.game.zw.resource.constant.GameError;
 import com.gryphpoem.game.zw.resource.domain.Player;
+import com.gryphpoem.game.zw.resource.pojo.Mail;
+import com.gryphpoem.game.zw.resource.util.CheckNull;
 
 /**
  * Description:
@@ -14,7 +18,11 @@ import com.gryphpoem.game.zw.resource.domain.Player;
 public class GetMailReportHandler extends AsyncGameHandler {
     @Override
     public void action() throws Exception {
+        GamePb5.GetMailReportRq req = msg.getExtension(GamePb5.GetMailReportRq.ext);
         Player player = DataResource.ac.getBean(PlayerDataManager.class).checkPlayerIsExist(getRoleId());
-        player.mails.get(msg.getExtension(GamePb5.GetMailReportRq.ext));
+        Mail mail = player.mails.get(req.getMailKeyId());
+        if (CheckNull.isNull(mail)) {
+            throw new MwException(GameError.MAIL_NOT_EXIST.getCode(), "获取邮件列表为空, roleId:", getRoleId());
+        }
     }
 }
