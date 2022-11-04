7507585294d72cbfb9fd1046c3f957cd9192a880
zhangpeng <0807zp123>
2022-11-04 19:14:53 +0800

    三国2战斗重置 - 连击效果未实现类提交

---
+++ mw_fight/src/main/java/com/gryphpoem/game/zw/buff/impl/effect/DoubleHitEffectImpl.java
@@ -0,0 +1,45 @@
+package com.gryphpoem.game.zw.buff.impl.effect;
+
+import com.gryphpoem.game.zw.buff.IFightBuff;
+import com.gryphpoem.game.zw.buff.abs.effect.AbsFightEffect;
+import com.gryphpoem.game.zw.constant.FightConstant;
+import com.gryphpoem.game.zw.manager.annotation.BuffEffectType;
+import com.gryphpoem.game.zw.pojo.p.FightBuffEffect;
+import com.gryphpoem.game.zw.pojo.p.FightContextHolder;
+import com.gryphpoem.game.zw.pojo.p.FightEffectData;
+import com.gryphpoem.game.zw.pojo.p.Force;
+
+import java.util.List;
+
+/**
+ * Description:
+ * Author: zhangpeng
+ * createTime: 2022-11-04 19:14
+ */
+@BuffEffectType(buffEffect = FightConstant.BuffEffect.EFFECT)
+public class DoubleHitEffectImpl extends AbsFightEffect {
+    @Override
+    public int[] effectType() {
+        return new int[0];
+    }
+
+    @Override
+    public IFightBuff compareTo(List sameIdBuffList, List effectConfig, FightBuffEffect fightBuffEffect, FightContextHolder contextHolder) {
+        return null;
+    }
+
+    @Override
+    protected boolean compareValue(Force actingForce, int actingHeroId, int effectLogicId, Object... params) {
+        return false;
+    }
+
+    @Override
+    protected double calValue(Force force, int heroId, int effectLogicId, Object... params) {
+        return 0;
+    }
+
+    @Override
+    protected FightEffectData createFightEffectData(IFightBuff fightBuff, List<Integer> effectConfig, FightBuffEffect fbe) {
+        return null;
+    }
+}
